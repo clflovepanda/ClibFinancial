f729fc7de13245924391e6daeb5a3ec9d15457e3
changlifeng <changlifeng@meituan.com>
2016-11-25 13:58:40 +0800

    ClibFinancial构建项目

--- src/main/java/com/clibchina/financial/controller/IndexController.java
+++ src/main/java/com/clibchina/financial/controller/IndexController.java
@@ -1,23 +1,16 @@
 package com.clibchina.financial.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Created by changlifeng on 16/11/25.
  */
-@Controller
+//@Controller
 public class IndexController {
 
-    @RequestMapping(value = "")
-    @ResponseBody
-    public Map<String, Object> index() {
-        Map<String, Object> result = new HashMap<>();
-        result.put("status", "ok");
-        return result;
-    }
+//    @RequestMapping(value = "")
+//    @ResponseBody
+//    public Map<String, Object> index() {
+//        Map<String, Object> result = new HashMap<>();
+//        result.put("status", "ok");
+//        return result;
+//    }
 }
